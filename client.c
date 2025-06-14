11d4a3bd2b2deace7a5fca045b8ca469d44ed027
Choudharihimanshu <77497284+Choudharihimanshu@users.noreply.github.com>
2025-06-14 15:52:07 +0530

    Update client.c

--- client.c
+++ client.c
@@ -1,4 +1,3 @@
-// client.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,52 +5,51 @@
 #include <pthread.h>
 #include <arpa/inet.h>
 
-#define BUF_SIZE 1024
-#define PORT 8080
+#define BUFFER_SIZE 1024
 
-int sock;
+int sockfd;
 
-void *receive_handler(void *arg) {
-    char buffer[BUF_SIZE];
-    int len;
-
-    while ((len = recv(sock, buffer, BUF_SIZE, 0)) > 0) {
-        buffer[len] = '\0';
+void *receive_messages(void *arg) {
+    char buffer[BUFFER_SIZE];
+    while (1) {
+        int bytes_received = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
+        if (bytes_received <= 0) {
+            printf("Disconnected from server.\n");
+            exit(1);
+        }
+        buffer[bytes_received] = '\0';
         printf("%s", buffer);
+        fflush(stdout);
     }
-
     return NULL;
 }
 
 int main() {
     struct sockaddr_in server_addr;
-    char buffer[BUF_SIZE];
-    pthread_t recv_thread;
+    char buffer[BUFFER_SIZE];
+    char name[50];
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        perror("Socket error");
-        return 1;
-    }
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strcspn(name, "\n")] = '\0'; // remove newline
 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Connect failed");
-        return 1;
-    }
+    connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
-    printf("Connected to chat server.\n");
+    // Send name to server
+    send(sockfd, name, strlen(name), 0);
 
-    pthread_create(&recv_thread, NULL, receive_handler, NULL);
+    pthread_t recv_thread;
+    pthread_create(&recv_thread, NULL, receive_messages, NULL);
 
     while (1) {
-        fgets(buffer, BUF_SIZE, stdin);
-        send(sock, buffer, strlen(buffer), 0);
+        fgets(buffer, sizeof(buffer), stdin);
+        send(sockfd, buffer, strlen(buffer), 0);
     }
 
-    close(sock);
     return 0;
 }
